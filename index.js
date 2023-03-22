65c299f7a77367fb9ecc7621d1f79d51d082167f
Andrew OBrien <aobrienaf@gmail.com>
2023-03-22 18:17:17 -0500

    Andrews javascript functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') { 
+    return `This Saturday, I want to ${activity}!`  
+}
+
+function mondayWork(descision = 'go to the office') { 
+    return `This Monday, I will ${descision}.`  
+}
+function wrapAdjective(wrap = '*') {
+    return function(adjective = 'special') {
+        return `You are ${wrap}${adjective}${wrap}!`
+    }
+
+}
